1895942ee6761239a529b21fcbd98b9e96f2febf
Yaemil <yaeflores@gmail.com>
2016-01-28 15:33:35 -0430

    Update Tarjeta_Red.c

--- Tarjeta_Red.c
+++ Tarjeta_Red.c
@@ -1,18 +1,12 @@
-/**
- * Nuestro Modulo del Proyecto mas que todo son procedimientos los cuales son invocados por el
- * manejador de interrupciones el cual nos indicara cuando recibir y cuando enviar datos a traves
- * de la Tarjeta de Red, y a su vez dependeremos del Firewall el cual este tendra la capacidad de 
- * poder bloquear el envio y recepcion de paquetes. Al inicio se coloco un Random (0 - 1), el cual 
- * simulara las interrupciones 0 Recepcion y 1 envio de paquetes...
-**/
-#include <stdio.h>
+#include <stdio.h>		///Falta mejorar la Sincronizacion y Señalizacion...///
+/**Tarjeta de Red**/
 #include <time.h>
 #include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
-#include <stdlib.h>
 
-#define MAX 21
+#define MAX 20
+#define N 10		// Numero de Hilos a Crear...//
 #define MOD %
 #define AND &&
 #define OR ||
@@ -23,74 +17,46 @@ typedef int bool;
 
 typedef struct
 {
-	int Buffer;
-	sem_t Bloqueado;
-}Tarjeta_Red;
+	int Buffer_In, Buffer_Out;
+	sem_t Bloqueado;// semaforo  usado por el firewall
+	sem_t total; //semaforo para el manejador de interrupciones
+	sem_t In, Out;
+}Tarjeta_Red;	///Los semaforos ueden colocarse de forma global...///
 
-pthread_mutex_t Mutex_Requerido = PTHREAD_MUTEX_INITIALIZER;	// Garantiza EM //
-pthread_cond_t Cond_Tarjeta_Disponible = PTHREAD_COND_INITIALIZER;	//Semaforo Condicion que duerme miestras no que el Buffer este lleno.//
-
-void Inicio(Tarjeta_Red *TR);
-void Envio_Recepcion_Datos(Tarjeta_Red *TR, int IRQ);
+int Prioridad_Proceso(pthread_t Hilo);
 void Cierre_Envio_Recepcion_Tarjeta_Red(Tarjeta_Red *TR);
 void Habilitacion_Envio_Recepcion_Tarjeta_Red(Tarjeta_Red *TR);
-void *Recepcion_Peticion(Tarjeta_Red *TR);
-void Insertar_Peticion_Buffer(Tarjeta_Red *TR);
-void *Envio_Peticion(Tarjeta_Red *TR);
-void Eliminar_Peticion_Buffer(Tarjeta_Red *TR);
+void Insertar_Peticion_Buffer(int *Buffer);
+void Envio_Peticion(int *Buffer);
+void *Eliminar_Peticion_Buffer(int *Buffer);
+void Liberar_Buffer(int *Buffer);
+void Solicitar_Tarjeta_Red(Tarjeta_Red *TR);
 
 int main()
 {
+	int i;
+	pthread_t Paquetes[N];
+	pthread_attr_t Hilo;
+	struct sched_param fifo_param; 
 	Tarjeta_Red TR;
 	
-	TR.Buffer = 0;
+	TR.Buffer_In = 0;
+	TR.Buffer_Out = 0;
 	sem_init(&TR.Bloqueado, 0, 1);
-	Inicio(&TR);
+	sem_init(&TR.In, 0, 1);
+	sem_init(&TR.Out, 0, 1);
+	sem_wait(&TR.Bloqueado);
+	sem_wait(&TR.In);
+	sem_wait(&TR.Out);
+	Solicitar_Tarjeta_Red(&TR);
 	
 	return 0;
 }
 
-void Inicio(Tarjeta_Red *TR)
-{
-	
-	int Num;
-	srand(time(NULL));
-	while(1)
-	{
-		Num = (rand() MOD 2);
-		Cierre_Envio_Recepcion_Tarjeta_Red(TR);   // Este es invocado por el Firewall para bloquear la Tarjeta de Red.//
-		Habilitacion_Envio_Recepcion_Tarjeta_Red(TR);
-		Envio_Recepcion_Datos(TR, Num);
-	}
-}
-
-//	Cuando ocurre una IRQ5...	//
-void Envio_Recepcion_Datos(Tarjeta_Red *TR, int IRQ)	///Falta una buena sincronizacion aqui...///
-{
-	int Valor;
-	pthread_t P1, P2;
-	//~ printf("\nIRQ ->: %d\n", IRQ);
-	pthread_mutex_lock(&Mutex_Requerido);
-	if((sem_getvalue(&TR->Bloqueado, &Valor) == 0) AND (Valor == 1))
-	{
-		if(IRQ == 0)
-		{
-			Recepcion_Peticion(TR);
-		}else{
-			Envio_Peticion(TR);
-		}
-		pthread_mutex_unlock(&Mutex_Requerido);	// Desbloquea el semaforo "Mutex_Requerido"	//
-		pthread_cond_signal(&Cond_Tarjeta_Disponible);
-	}else{
-		printf("\nFirewall Bloqueo el Envio y Recepcion de Datos...\n");
-		system("sleep 2.0");
-	}
-}
-
-//	Cuando ocurre una IRQ6...	//
+//	Cuando ocurre una IRQ6...	esto deberia se manejado por el modulo de interrupciones creo //
 void Cierre_Envio_Recepcion_Tarjeta_Red(Tarjeta_Red *TR)
 {
-	sem_wait(&TR->Bloqueado);
+	sem_wait(&TR->Bloqueado);// deberia estar ser el nuevo semaforo
 }
 
 void Habilitacion_Envio_Recepcion_Tarjeta_Red(Tarjeta_Red *TR)
@@ -98,40 +64,97 @@ void Habilitacion_Envio_Recepcion_Tarjeta_Red(Tarjeta_Red *TR)
 	sem_post(&TR->Bloqueado);
 }
 
-void *Recepcion_Peticion(Tarjeta_Red *TR)
+void Insertar_Peticion_Buffer(int *Buffer)
 {
-	if(TR->Buffer < MAX)
+	*Buffer = (*Buffer + 1);
+	printf("\nNuevo Paquete a Enviar...\n");
+	system("sleep 1.0");
+}
+
+void Envio_Peticion(int *Buffer)
+{
+	if(*Buffer < MAX)
 	{
-		Insertar_Peticion_Buffer(TR);
+		printf("\nPaquete Guardado en el Buffer...\n");
+		system("sleep 1.0");
+		Insertar_Peticion_Buffer(Buffer);
 	}else{
 		printf("\nBuffer LLeno...\n");
 		system("sleep 2.0");
 	}
 }
 
-void Insertar_Peticion_Buffer(Tarjeta_Red *TR)
+void *Eliminar_Peticion_Buffer(int *Buffer) // Parametro *Buffer
 {
-	TR->Buffer = (TR->Buffer + 1);
-	printf("\nNuevo Paquete, para enviar...\n");
-	printf("Capacidad Buffer ->: %d\n", TR->Buffer);
-	system("sleep 1.0");
-}
-
-void *Envio_Peticion(Tarjeta_Red *TR)
-{
-	if(TR->Buffer >  0)
+	if(*Buffer > 0)
 	{
-		Eliminar_Peticion_Buffer(TR);
-	}else{
-		printf("\nBuffer Vacio...\n");
-		system("sleep 2.0");
+		printf("\nLiberando Buffer...\n");
+		system("sleep 1.0");
+		Liberar_Buffer(Buffer);
 	}
 }
 
-void Eliminar_Peticion_Buffer(Tarjeta_Red *TR)
+void Liberar_Buffer(int *Buffer) // Parametro *Buffer
 {
-	TR->Buffer = (TR->Buffer - 1);
-	printf("\nPaquete, enviado...\n");
-	printf("Capacidad Buffer ->: %d\n", TR->Buffer);
+	*Buffer = (*Buffer - 1);
+	printf("\nPaquete enviado...\n");
+	printf("\nBuffer Liberado...\n");
 	system("sleep 1.0");
+}
+
+void Solicitar_Tarjeta_Red(Tarjeta_Red *TR)
+{
+	int Valor;
+	if((sem_getvalue(&TR->Bloqueado, &Valor) == 0) AND (Valor == 1))	//Si el firewall no esta bloqueado puedo recibir y enviar de una vez...//
+	{
+		if((sem_getvalue(&TR->In, &Valor) == 0) AND (Valor == 0))	///Si valor es igual 0, entonces esa es la señal de que es un paquete de entrada.///
+		{
+			printf("\n\t*Paquete de Entrada*\n");
+			system("sleep 1.0");
+			Envio_Peticion(&TR->Buffer_In);
+			printf("\nCapacidad del Buffer Entrada ->: %d\n", TR->Buffer_In);
+			system("sleep 1.0");
+			Eliminar_Peticion_Buffer(&TR->Buffer_In);
+			printf("\nCapacidad del Buffer Entrada ->: %d\n", TR->Buffer_In);
+			system("sleep 1.0");
+			sem_post(&TR->In);
+		}
+		if((sem_getvalue(&TR->Out, &Valor) == 0) AND (Valor == 0))	///Si valor es igual 0, entonces esa es la señal de que es un paquete de salida.///
+		{
+			printf("\n\t*Paquete de Salida*\n");
+			system("sleep 1.0");
+			Envio_Peticion(&TR->Buffer_Out);
+			printf("\nCapacidad del Buffer Salida ->: %d\n", TR->Buffer_Out);
+			system("sleep 1.0");
+			Eliminar_Peticion_Buffer(&TR->Buffer_Out);
+			printf("\nCapacidad del Buffer Salida ->: %d\n", TR->Buffer_Out);
+			system("sleep 1.0");
+			sem_post(&TR->Out);
+		}
+	}else{
+		printf("\nFirewall Bloqueo el Envio y Recepcion de Datos...\n");	///En este caso el Firewall bloqueo la tarjeta de Red, pero el igual puede recibir los paquetes pero no los envia...///
+		system("sleep 2.0");
+		if((sem_getvalue(&TR->In, &Valor) == 0) AND (Valor == 0))
+		{
+			printf("\n\t*Paquete de Entrada*\n");
+			system("sleep 1.0");
+			Envio_Peticion(&TR->Buffer_In);
+			printf("\nCapacidad del Buffer Entrada ->: %d\n", TR->Buffer_In);
+			system("sleep 1.0");
+			printf("\nNo se puede enviar porque el Firewall bloqueo la _Tarjeta de Red...\n");
+			system("sleep 1.0");
+			sem_post(&TR->In);
+		}
+		if((sem_getvalue(&TR->Out, &Valor) == 0) AND (Valor == 0))
+		{
+			printf("\n\t*Paquete de Salida*\n");
+			system("sleep 1.0");
+			Envio_Peticion(&TR->Buffer_Out);
+			printf("\nCapacidad del Buffer Salida ->: %d\n", TR->Buffer_Out);
+			system("sleep 1.0");
+			printf("\nNo se puede enviar porque el Firewall bloqueo la _Tarjeta de Red...\n");
+			system("sleep 1.0");
+			sem_post(&TR->Out);
+		}
+	}
 }
